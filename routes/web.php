f10c4d63eb8db3bdc7e7570d98bdad901be3b954
unknown <chrism4rsh@gmail.com>
2021-01-19 14:04:57 +0000

    modify file path

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\controllers\NewController;
+use app\Http\controllers\NewController;
 
 /*
 |--------------------------------------------------------------------------
